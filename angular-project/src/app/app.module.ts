9ade6d6259d98d640d97a019ac98f582d00cd862
Atanas Chestojanov <ac7723@student.uni-lj.si>
2024-04-20 17:00:15 +0200

    versusPage created

--- angular-project/src/app/app.module.ts
+++ angular-project/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { VersusPageComponent } from './versus-page/versus-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    VersusPageComponent
   ],
   imports: [
     BrowserModule,
