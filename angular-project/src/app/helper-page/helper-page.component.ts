5a4111226f9c1dafaf0f0a2b29752180d8ecbbf2
Atanas Chestojanov <ac7723@student.uni-lj.si>
2024-04-20 16:21:23 +0200

    helper page setup

---
+++ angular-project/src/app/helper-page/helper-page.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-helper-page',
+  templateUrl: './helper-page.component.html',
+  styleUrl: './helper-page.component.css'
+})
+export class HelperPageComponent {
+
+}
