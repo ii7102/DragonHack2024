b624e3df765438cd181ccccf080686f0c2820ea2
sivanovska <si3840@student.uni-lj.si>
2024-04-20 21:55:52 +0200

    folder components

--- angular-project/src/app/app.module.ts
+++ angular-project/src/app/app.module.ts
@@ -5,9 +5,9 @@ import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InsertWordComponent } from './components/insert-word/insert-word.component';
-import { VersusPageComponent } from './versus-page/versus-page.component';
-import { ResultBoxComponent } from './result-box/result-box.component';
-import { HelperPageComponent } from './helper-page/helper-page.component';
+import { VersusPageComponent } from './components/versus-page/versus-page.component';
+import { ResultBoxComponent } from './components/result-box/result-box.component';
+import { HelperPageComponent } from './components/helper-page/helper-page.component';
 
 @NgModule({
   declarations: [
