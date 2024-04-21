7f7e65a7b448a368b2263e48684c037f57eb7b9a
sivanovska <si3840@student.uni-lj.si>
2024-04-21 08:46:36 +0200

    routing

--- angular-project/src/app/components/navbar/navbar.component.ts
+++ angular-project/src/app/components/navbar/navbar.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.css'
+  styleUrls: ['./navbar.component.css'] // <-- Change styleUrl to styleUrls
 })
 export class NavbarComponent {
+  @Output() asistMe = new EventEmitter<void>();
+  @Output() playMe=  new EventEmitter<void>();
 
+  assistWordle(){
+    this.asistMe.emit();
+  }
+
+  playWordle(){
+    this.playMe.emit();
+  }
 }
